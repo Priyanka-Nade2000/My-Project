eafe6e660578e6a3cff81e4fa694b126bd2e522e
Priyanka-Nade2000 <nadeps17@gmail.com>
2024-04-03 23:00:24 +0530

    first commit

---
+++ src/state/store/store.js
@@ -0,0 +1,8 @@
+
+import { applyMiddleware, compose, createStore } from "redux";
+import rootreducer from "../reducer";
+import { thunk } from "redux-thunk";
+
+const store=createStore(rootreducer,compose(applyMiddleware(thunk)))
+
+export default store;
